a5098ae239b15789103a1985ad121c794598d92b
Nekoto <81257780+nekotogd@users.noreply.github.com>
2022-11-08 23:20:46 +0400

    Navbar dynamic transparency + various CSS fixes

--- commons/navbar_active.js
+++ commons/navbar_active.js
@@ -5,3 +5,15 @@ hamburger.addEventListener("click", function () {
 	hamburger.classList.toggle("nav-active");
 	mobile_menu.classList.toggle("nav-active");
 });
+
+var navroot = document.querySelector(".navroot");
+window.addEventListener("scroll", function () {
+	if (window.scrollY == 0) {
+		//user is at the top of the page; no need to show the back to top button
+		navroot.classList.remove("scrolled");
+	} else {
+		if (!navroot.classList.contains("scrolled")) {
+			navroot.classList.add("scrolled");
+		}
+	}
+});
